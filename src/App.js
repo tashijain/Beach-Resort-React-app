5892bbe3d95e86a2c54ce6b6ea1938a30740de33
Tashi Jain <tashjain88@gmail.com>
2020-07-29 12:54:21 -0700

    initial setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <>hello from app</>;
+}
+
+export default App;
---
+++ src/pages/Rooms.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Rooms() {
+  return <div>hello from rooms page</div>;
+}

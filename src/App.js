8da30738fda8dde2c0746d2c9b8a67633c4e4c61
Tashi Jain <tashjain88@gmail.com>
2020-07-29 13:28:47 -0700

    router, switch setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
 
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
+
 function App() {
-  return <>hello from app</>;
+  return (
+    <>
+      {/* switch renders the first route that matches/includes the path 
+      a route with no path always matches so Error is default page shown*/}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        {/* slug is a route parameter */}
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;

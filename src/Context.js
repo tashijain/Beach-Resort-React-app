aa3ce1fbc833f3d1e648043f9ef2579b66934101
Tashi Jain <tashjain88@gmail.com>
2020-07-29 21:13:56 -0700

    loading component

--- src/Context.js
+++ src/Context.js
@@ -1,13 +1,49 @@
 import React, { Component } from "react";
+import items from "./data";
 
 // Context API: Context provides a way to pass data through the component tree without having to pass props down manually at every level.
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
   state = {
-    greeting: "hello",
-    name: "john",
+    // to store all rooms
+    rooms: [],
+    // to store rooms sorted on criteria
+    sortedRooms: [],
+    // to store featured rooms
+    featuredRooms: [],
+    // not needed if data stored locally but needed
+    // if get from server(externally) so we don't render
+    // until loading is false
+    loading: true,
   };
+
+  //getData
+
+  // lifecycle method
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    let featuredRooms = rooms.filter((room) => room.featured === true);
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false,
+    });
+  }
+
+  formatData(items) {
+    let tempItems = items.map((item) => {
+      let id = item.sys.id;
+      // now only image urls
+      let images = item.fields.images.map((image) => image.fields.file.url);
+      // spread operator to copy all properties from fields object
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
+
   render() {
     return (
       <RoomContext.Provider value={{ ...this.state }}>
--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import { RoomContext } from "../Context";
+import Loading from "./Loading";
+import Room from "./Room";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { name, greeting } = this.context;
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
     return (
       <div>
-        {greeting} {name} from featured rooms
+        from featured rooms
+        <Room />
+        <Loading />
       </div>
     );
   }

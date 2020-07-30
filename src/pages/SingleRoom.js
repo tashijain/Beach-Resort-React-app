9c36423a9c42d92bddc8403c599ffac844e94338
Tashi Jain <tashjain88@gmail.com>
2020-07-29 23:12:13 -0700

    styled components dynamic img

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../Context";
+import StyledHero from "../components/StyledHero";
 
 export default class SingleRoom extends Component {
   // prop being passed by react router: history, location, match
@@ -44,13 +45,13 @@ export default class SingleRoom extends Component {
       images,
     } = room;
     return (
-      <Hero hero="roomsHero">
+      <StyledHero img={images[0] || this.state.defaultBcg}>
         <Banner title={`${name} room`}>
           <Link to="/rooms" className="btn-primary">
             Back to rooms
           </Link>
         </Banner>
-      </Hero>
+      </StyledHero>
     );
   }
 }

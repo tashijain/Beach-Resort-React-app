00978e44bef3ba600781ec4ce4902e2b380a3b79
Tashi Jain <tashjain88@gmail.com>
2020-07-30 11:19:18 -0700

    single room page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -44,14 +44,55 @@ export default class SingleRoom extends Component {
       pets,
       images,
     } = room;
+
+    // rest operator to get all the others - array destructuring
+    // The destructuring assignment syntax is a JavaScript expression
+    // that makes it possible to unpack values from arrays, or properties
+    //  from objects, into distinct variables.
+    // used to display all images except first one since it's shown in banner
+    const [mainImg, ...restImgs] = images;
     return (
-      <StyledHero img={images[0] || this.state.defaultBcg}>
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            Back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              Back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {restImgs.map((item, index) => {
+              return <img key={index} src={item} alt={name} />;
+            })}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price : ${price}</h6>
+              <h6>size : {size} SQFT</h6>
+              <h6>
+                max capacity :{" "}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+              <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, index) => {
+              return <li key={index}>- {item}</li>;
+            })}
+          </ul>
+        </section>
+      </>
     );
   }
 }

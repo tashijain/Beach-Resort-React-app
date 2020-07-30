9fd133da124048a2f7516d059a01677012c02781
Tashi Jain <tashjain88@gmail.com>
2020-07-30 13:02:15 -0700

    rooms list

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Room from "./Room";
 
-export default function RoomList() {
-  return <div>hello room list</div>;
+export default function RoomList({ rooms }) {
+  if (rooms.length == 0) {
+    return (
+      <div className="empty-search">
+        <h3>unfortunately no rooms matched your search criteria</h3>
+      </div>
+    );
+  }
+  return (
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {rooms.map((item) => {
+          return <Room key={item.id} room={item} />;
+        })}
+      </div>
+    </section>
+  );
 }

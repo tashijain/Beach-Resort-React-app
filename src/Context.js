396dbb19259abd0f2a1341be4d621ddfa622b94e
Tashi Jain <tashjain88@gmail.com>
2020-07-31 14:40:59 -0700

    room filter on room type

--- src/Context.js
+++ src/Context.js
@@ -16,6 +16,15 @@ class RoomProvider extends Component {
     // if get from server(externally) so we don't render
     // until loading is false
     loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   //getData
@@ -24,11 +33,16 @@ class RoomProvider extends Component {
   componentDidMount() {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter((room) => room.featured === true);
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   }
 
@@ -53,9 +67,51 @@ class RoomProvider extends Component {
     return room;
   };
 
+  handleChange = (event) => {
+    const target = event.target;
+    // since we also have the checkbox for which we handle change
+    const value = event.type === "checkbox" ? target.checked : target.value;
+    const name = event.target.name;
+    // filter rooms is callback fn
+    this.setState(
+      {
+        // don't use if else checks as hardcoding to prevent recoding and reploying this code if data changes
+        [name]: value,
+      },
+      this.filterRooms
+    );
+  };
+
+  filterRooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets,
+    } = this.state;
+
+    let tempRooms = [...rooms];
+    if (type !== "all") {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+    this.setState({
+      sortedRooms: tempRooms,
+    });
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange,
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../Context";
+import Title from "./Title";
 
 // hook used to access ocntext within a functional component
 
-export default function RoomFilter() {
-  return <div>hello from rooms filter</div>;
+// function that gets unique values for room type since there
+// are manyy single, double rooms but show option only once
+const getUnique = (items, value) => {
+  // sets don't contain duplicate
+  return [...new Set(items.map((item) => item[value]))];
+};
+
+export default function RoomFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+  // get Unique types
+  let types = getUnique(rooms, "type");
+  // add all as a type too
+  types = ["all", ...types];
+  // map to JSX
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+  return (
+    <section className="filter-container">
+      <Title title="search room" />
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {/* could hard-code it but data can change any time */}
+            {/* <option value="single" >single</option> */}
+            {types}
+          </select>
+        </div>
+        {/* end of select type */}
+      </form>
+    </section>
+  );
 }
